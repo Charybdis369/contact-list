e9fcd4c3865cf9dfdc966ceabe514d39f9cf2794
richard <ricstewa41@cox.net>
2024-02-21 12:10:49 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+const APIURL = 'https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users'
+import { useState, effectState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
