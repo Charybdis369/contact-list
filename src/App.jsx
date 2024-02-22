d0c29072c4cccb4bf4ba813e6168968eb8d908ff
richard <ricstewa41@cox.net>
2024-02-21 19:55:44 -0600

    worked on selected contact view but couldnt figure it out

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 
 import { useState } from 'react'
 import ContactList from './components/ContactList'
-//import SelectedContact from './components/SelectedContact'
+
 import './App.css'
 
 function App() {
- //const [selectedCOntactId, setSelectedContactId] = useState(null)
+ 
 
   return (
     <>
+    
       <ContactList />
     </>
   )
